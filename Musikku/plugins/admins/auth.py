1e90804e9db9aad427195f1b82cf8e5fe3533b5c
milokun <102670609+milokun@users.noreply.github.com>
2022-05-15 20:18:23 +0700

    Update auth.py

--- Musikku/plugins/admins/auth.py
+++ Musikku/plugins/admins/auth.py
@@ -1,9 +1,9 @@
 #
-# Copyright (C) 2021-2022 by kenkansaja@Github, < https://github.com/kenkansaja >.
+# Copyright (C) 2021-2022 by milokun@Github, < https://github.com/milokun >.
 #
-# This file is part of < https://github.com/kenkansaja/Musikku > project,
+# This file is part of < https://github.com/milokun/Milomusic > project,
 # and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/kenkansaja/Musikku/blob/master/LICENSE >
+# Please see < https://github.com/milokun/Milomusic/blob/master/LICENSE >
 #
 # All rights reserved.
 
